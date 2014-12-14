b9c70b05a8c90f86313aa254fafae5211dd114ee
motoy3d <motoy3d@gmail.com>
2014-12-15 04:11:12 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'reysol'
+    ,hashtag: 'reysol'
+    ,teamName: '柏'
+    ,appName: 'スマートJ for 柏レイソル'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id929041384?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.reysol'
+    ,color: '#fff100'
+    ,tintColor: 'black'
+    ,dbName: 'smartj.reysol'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=reysol&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=reysol"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 244700
+    ,nendApiKeyIPhoneIcon: '154088c82ddc0118af9010cec4ab73739e6e6c2c'
+    ,nendSpotIdIPhoneBanner: 244699
+    ,nendApiKeyIPhoneBanner: 'fe9e7c2942a90107ae173ec8a37248e9c4e09da2'
+    //nend(Android)
+    ,nendSpotIdAndroid: 244698
+    ,nendApiKeyAndroid: '3d96009311d0da43bacce742f6b1e3f75f42eea1'
+};
